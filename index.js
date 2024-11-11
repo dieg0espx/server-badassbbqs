63fe66e8e99b403a71e5f1fbd35c8233b95228b9
dieg0espx <espinosa9mx@gmail.com>
2024-11-10 20:15:52 -0800

    .

---
+++ index.js
@@ -0,0 +1,73 @@
+import dotenv from 'dotenv';
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import axios from 'axios';
+
+dotenv.config();
+
+const app = express();
+app.use(cors({ origin: 'http://localhost:3000' }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+app.post('/api/confirm', async (req, res) => {
+    // const checkoutToken = req.body.checkout_token;
+    console.log(req.body);
+    
+  
+    // try {
+    //   // Make a request to Affirm to authorize the charge
+    //   const response = await axios.post('https://sandbox.affirm.com/api/v2/charges', {
+    //     checkout_token: checkoutToken,
+    //   }, {
+    //     headers: {
+    //       Authorization: `Bearer xdLBMW2WoNSmbwjKN9WFh1jVWm3EVEz5`,
+    //       'Content-Type': 'application/json',
+    //     },
+    //   });
+    //   // Handle success
+    //   res.status(200).json({ message: 'Payment authorized', data: response.data });
+    // } catch (error) {
+    //   console.error('Error authorizing payment:', JSON.stringify(error));
+    //   res.status(500).json({ message: 'Payment authorization failed', error: error.message });
+    // }
+  });
+
+
+  app.post('/api/test-affirm', async (req, res) => {
+    console.log("Affirm API Key:", process.env.AFFIRM_PRIVATE_API_KEY); // Debug log
+  
+    try {
+      const checkoutToken = '0TKTNYJ36HZLZKCP';
+  
+      const response = await axios.post(
+        'https://sandbox.affirm.com/api/v2/transactions',
+        { checkout_token: checkoutToken },
+        {
+          headers: {
+            Authorization: `Bearer ${process.env.AFFIRM_PRIVATE_API_KEY}`,
+            'Content-Type': 'application/json',
+          },
+        }
+      );
+  
+      res.status(200).json({ message: 'Authorization successful', data: response.data });
+    } catch (error) {
+      console.error('Error authorizing payment:', error.response?.data || error.message);
+      res.status(500).json({
+        message: 'Payment authorization failed',
+        error: error.response?.data || error.message,
+      });
+    }
+  })
+  
+  
+app.listen(8080, () => {
+    console.log(`Server running on http://localhost:8080`);
+});
+
+
+
+
